57522874c8281b01c7edd2505eefcebe1886b759
braedynl <braedynlettinga@gmail.com>
2022-12-03 21:01:18 -0500

    Use built-in `slice` instead of `range`

--- views/views.py
+++ views/views.py
@@ -1,5 +1,7 @@
+import reprlib
+import typing
 from collections.abc import Iterator, Sequence
-from typing import Any, Optional, SupportsIndex, TypeVar, final, overload
+from typing import Any, Optional, SupportsIndex, TypeVar
 
 __all__ = ["View"]
 
@@ -7,32 +9,26 @@ T    = TypeVar("T")
 Self = TypeVar("Self", bound="View")
 
 
-def clamp(x: int, lower: int, upper: int) -> int:
-    return max(lower, min(upper, x))
-
-
-@final
 class View(Sequence[T]):
     """A dynamically-sized, read-only view into a `Sequence[T]` object
 
     Views are thin wrappers around a reference to some `Sequence[T]` (called
-    the "target"), and a `range` of indices to view from it (called the
+    the "target"), and a `slice` of indices to view from it (called the
     "window"). Alterations made to the target are reflected by its views.
 
     At creation time, a window can be provided to define the view object's
-    boundaries. Views can shrink and expand, but only within the space alloted
-    by the window. The view will only contain values if the window's starting
-    index is within range of the target.
+    boundaries. Views may shrink if its target shrinks - they may also expand
+    if the window defines non-integral boundaries. The window may only contain
+    integral or `NoneType` values.
     """
-
-    NULL_WINDOW: range = range(0, 0, 1)
 
     __slots__ = ("_target", "_window")
 
-    def __init__(self, target: Sequence[T], window: Optional[range] = None) -> None:
+    def __init__(self, target: Sequence[T], window: slice = slice(None)) -> None:
         self._target = target
-        self._window = range(len(target)) if window is None else window
+        self._window = window
 
+    @reprlib.recursive_repr(fillvalue="...")
     def __repr__(self) -> str:
         """Return a canonical representation of the view"""
         return f"{self.__class__.__name__}(target={self._target!r}, window={self._window!r})"
@@ -41,37 +37,45 @@ class View(Sequence[T]):
 
     def __len__(self) -> int:
         """Return the number of currently viewable items"""
-        return len(self.subwindow())
+        return len(range(*self.indices()))
 
-    @overload
+    @typing.overload
     def __getitem__(self: Self, key: SupportsIndex) -> T: ...
-    @overload
+    @typing.overload
     def __getitem__(self: Self, key: slice) -> Self: ...
 
     def __getitem__(self, key):
         """Return the element or sub-view corresponding to `key`
 
-        Note that slicing is performed relative to the "complete" window, as
-        opposed to the active sub-window.
+        Note that slicing is performed relative to the currently viewable
+        items, rather than the "potential" selection of items defined by the
+        window.
 
         Since a new `View` instance is made for slice arguments, this method
         guarantees constant-time performance.
         """
-        target = self._target
-        window = self._window[key]
         if isinstance(key, slice):
-            return self.__class__(target, window)
-        return target[window]
+            return self.__class__(self, key)
+        subkeys = range(*self.indices())
+        try:
+            subkey = subkeys[key]
+        except IndexError as error:
+            raise IndexError(f"index out of range of window") from error
+        else:
+            result = self._target[subkey]
+            return result
 
     def __iter__(self) -> Iterator[T]:
         """Return an iterator that yields the currently viewable items"""
-        yield from map(self._target.__getitem__, self.subwindow())
+        subkeys = range(*self.indices())
+        yield from map(self._target.__getitem__, subkeys)
 
     def __reversed__(self) -> Iterator[T]:
         """Return an iterator that yields the currently viewable items in
         reverse order
         """
-        yield from map(self._target.__getitem__, reversed(self.subwindow()))
+        subkeys = range(*self.indices())
+        yield from map(self._target.__getitem__, reversed(subkeys))
 
     def __contains__(self, value: Any) -> bool:
         """Return true if the currently viewable items contains `value`,
@@ -88,46 +92,40 @@ class View(Sequence[T]):
     def __eq__(self, other: Any) -> bool:
         """Return true if the two views are equal, otherwise false
 
-        Views are considered equal if they refer to the same target and have
-        equal windows.
+        Views compare equal if they are element-wise equivalent, independent of
+        their target classes.
         """
         if isinstance(other, View):
-            return (
-                self._target is other._target
-                and
-                self._window == other._window
+            self_subkeys, other_subkeys = (
+                range( *self.indices()),
+                range(*other.indices()),
             )
+            if len(self_subkeys) != len(other_subkeys):
+                return False
+            return all(map(
+                lambda x, y: x is y or x == y,
+                map(
+                     self._target.__getitem__,
+                     self_subkeys,
+                ),
+                map(
+                    other._target.__getitem__,
+                    other_subkeys,
+                ),
+            ))
         return NotImplemented
 
     @property
-    def window(self) -> range:
-        """A range of potential indices to use in retrieval of target items"""
+    def window(self) -> slice:
+        """A slice of potential indices to use in retrieval of target items"""
         return self._window
 
-    def full(self) -> bool:
-        """Return true if the view is at full capacity, otherwise false
+    def indices(self) -> tuple[int, int, int]:
+        """Return the start, stop, and step indices that currently form the
+        viewable selection of the target
 
-        Equivalent to `len(view.window) == len(view)`.
+        Internally, these values are simply calculated from the window's
+        `indices()` method, using the length of the target sequence as its
+        argument.
         """
-        return len(self._window) == len(self)
-
-    def subwindow(self) -> range:
-        """Return a range of indices currently viewable from the window
-
-        If the window's starting index is out of range of the target,
-        `range(0, 0)` is returned.
-        """
-        target = self._target
-        window = self._window
-
-        length = len(target)
-
-        start = window.start
-
-        if start < -length or start >= length:
-            return self.NULL_WINDOW
-
-        stop = clamp(window.stop, lower=-length - 1, upper=length)
-        step = window.step
-
-        return range(start, stop, step)
+        return self._window.indices(len(self._target))
